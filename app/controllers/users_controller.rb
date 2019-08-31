b7378c874f8548ef0ac4d23e40a0bec293de76ab
jie7206 <1524556052@qq.com>
2019-08-31 17:18:20 +0800

    add gravatar_for method to show email image

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,12 @@
 class UsersController < ApplicationController
 
-  def new
+  def show
+    @user = User.find(params[:id])
   end
-  
+
+  def new
+    @user = User.new
+    #debugger
+  end
+
 end
