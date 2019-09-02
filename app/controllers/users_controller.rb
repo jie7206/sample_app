2a37134b661795521f666495f6a0be5b4a8768f0
jie7206 <1524556052@qq.com>
2019-09-02 11:28:07 +0800

    Finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,4 +9,20 @@ class UsersController < ApplicationController
     #debugger
   end
 
+  def create
+    @user = User.new(user_params)
+    if @user.save # 处理注册成功的情况
+      flash[:success] = "欢迎您来到我的范例网站!"
+      redirect_to user_url(@user)
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
+
 end
---
+++ test/integration/users_signup_test.rb
@@ -0,0 +1,31 @@
+require 'test_helper'
+
+class UsersSignupTest < ActionDispatch::IntegrationTest
+
+  test "invalid signup information" do
+    get signup_path
+    assert_no_difference 'User.count' do
+        post signup_path,  params: { user: { name:  "test001",
+                                            email: "user@invalid.com",
+                                            password: "foofoo",
+                                            password_confirmation: "barbar" } }
+    end
+    assert_template 'users/new'
+    assert_select "div#error_explanation"
+    assert_select "div .field_with_errors", 2
+  end
+
+  test "valid signup information" do
+    get signup_path
+    assert_difference 'User.count', 1 do
+              post users_path, params: { user: { name:  "Example User",
+                                                 email: "user@example.com",
+                                                 password: "123456",
+                                                 password_confirmation: "123456" }}
+    end
+    follow_redirect!
+    assert_template 'users/show'
+    assert_select "div", "欢迎您来到我的范例网站!"
+  end
+
+end
