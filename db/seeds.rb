bc26bb4dd81c2874560c3a0dc4782386f5a5dbd1
jie7206 <1524556052@qq.com>
2019-09-11 09:38:24 +0800

    update codes

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,13 +9,17 @@ User.create!(name:  "Michael Lin",
              email: "1524556052@qq.com",
              password:              "123456",
              password_confirmation: "123456",
-             admin: true)
-200.times do |n|
+             admin: true,
+             activated: true,
+             activated_at: Time.zone.now )
+100.times do |n|
   name = Faker::Name.name
   email = "example-#{n+1}@railstutorial.org"
   password = "password"
   User.create!(name: name,
                email: email,
                password:              password,
-               password_confirmation: password)
+               password_confirmation: password,
+               activated: true,
+               activated_at: Time.zone.now)
 end
--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -2,6 +2,10 @@ require 'test_helper'
 
 class UsersSignupTest < ActionDispatch::IntegrationTest
 
+  def setup
+    ActionMailer::Base.deliveries.clear
+  end
+
   test "invalid signup information" do
     get signup_path
     assert_no_difference 'User.count' do
@@ -15,17 +19,31 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     assert_select "div .field_with_errors", 2
   end
 
-  test "valid signup information" do
+  test "valid signup information with account activation" do
     get signup_path
     assert_difference 'User.count', 1 do
-              post users_path, params: { user: { name:  "Example User",
-                                                 email: "user@example.com",
-                                                 password: "123456",
-                                                 password_confirmation: "123456" }}
+      post users_path, params: { user: { name:  "Example User",
+                                         email: "user@example.com",
+                                         password: "123456",
+                                         password_confirmation: "123456" }}
     end
+    assert_equal 1, ActionMailer::Base.deliveries.size
+    user = assigns(:user)
+    assert_not user.activated?
+    # 尝试在激活之前登录
+    log_in_as(user)
+    assert_not is_logged_in?
+    # 激活令牌无效
+    get edit_account_activation_path("invalid token", email: user.email)
+    assert_not is_logged_in?
+    # 令牌有效，电子邮件地址不对
+    get edit_account_activation_path(user.activation_token, email: 'wrong')
+    assert_not is_logged_in?
+    # 激活令牌有效
+    get edit_account_activation_path(user.activation_token, email: user.email)
+    assert user.reload.activated?
     follow_redirect!
     assert_template 'users/show'
-    assert_select "div", "欢迎您来到我的范例网站!"
     assert is_logged_in?
   end
 
